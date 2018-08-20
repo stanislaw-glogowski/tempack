e7234294d2dc4615d35adddc29e6da4c07185e08
Stanisław Głogowski <stanislaw.glogowski@gmail.com>
2018-08-20 07:38:15 +0200

    Updates logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,22 +1,27 @@
 import { green, cyan, red } from "colors/safe";
 import { IBuilderLogger } from "./builder";
 
-const print = (prefix, message) => {
-  console.log(`[ ${prefix} ] ${message}`);
+const PREFIX = "tempack";
+const PADDING_BASE = 8;
+
+const print = (color: (value: string) => string, type: string, message: any) => {
+  const padding = " ".repeat(PADDING_BASE - type.length);
+  console.log(`[ ${color(PREFIX)} ] [ ${color(type)} ]${padding}${message}`);
 };
 
 /**
  * builder logger instance
  */
 const logger: IBuilderLogger = {
-  success: (message) => print(green("tempack:success"), message),
-  info: (message) => print(cyan("tempack:info"), message),
+  success: (message) => print(green, "success", message),
+  info: (message) => print(cyan, "info", message),
   error: (err) => {
-    let message: string = "Unknown";
+    let message: string = "unknown";
     if (err instanceof Error) {
       message = (err as Error).message;
     }
-    print(red("tempack:error"), message);
+
+    print(red, "error", red(message));
   },
 };
 
